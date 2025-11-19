8ff461059a65f8380e94f59ce5ca2ee5a30628e1
Borong Zhang <mc45324@um.edu.mo>
2025-11-20 01:16:12 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'real_robot_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='borong',
+    maintainer_email='borong@todo.todo',
+    description='Real robot policy control package',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'policy_node = real_robot_pkg.policy_node:main',
+        ],
+    },
+)
